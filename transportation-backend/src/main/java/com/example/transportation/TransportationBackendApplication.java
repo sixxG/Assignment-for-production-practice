fb63fc6ff3900a89980b2d7d3cfebf633e6132c6
sixxG <oleg.titov.2002@gmail.com>
2023-03-14 11:09:41 +0300

    Создал шаблон проекта для backend части с основными зависимостями

---
+++ transportation-backend/src/main/java/com/example/transportation/TransportationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.transportation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportationBackendApplication.class, args);
+	}
+
+}
